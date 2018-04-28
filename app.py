4e578dd965c001e6efd6c9d0b66c896ca248e76e
Sam Keel <programming@vectors.net.au>
2018-04-28 22:05:14 +0800

    Added doubly linked lists

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import lists_pointers as lp
+import doubly_linked_list
 
+print("--- Singly Linked list start")
 # add new items to SinglyLinkedList
 words = lp.SinglyLinkedList()
 words.append('egg')
@@ -20,3 +22,20 @@ print("After deleting word, word count is: " + str(words.count))
 # Find a word in the list using the iter method. Returns True if found False if missing.
 if words.search('bacon'):
     print("found bacon in the list.")
+
+print("/--- Singly Linked list end")
+print("--- Doubly Linked list start")
+dll = doubly_linked_list.DoublyLinkedList()
+dll.append('Croissants')
+dll.append('Pies')
+dll.append('Danishes')
+dll.append('Macaroons')
+dll.append('Pretzels')
+print('- Printed forwards:')
+dll.print_forwards()
+print('- Printed backwards:')
+dll.print_backwards()
+print('Insert new item to front of list')
+dll.insert_head('honey cake')
+print('Get first element: {}'.format(dll[0]))
+print("/--- Doubly Linked list end")
