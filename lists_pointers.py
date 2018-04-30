58cd9a4500fc216541d387061475a6207c93c28c
Sam Keel <programming@vectors.net.au>
2018-04-30 20:40:42 +0800

    Update lists_pointers.py

--- lists_pointers.py
+++ lists_pointers.py
@@ -59,4 +59,24 @@ class SinglyLinkedList:
         return False
 
 
+class printSLL:
+    def __init__(self):
+        print("--- Singly Linked list start")
+        # add new items to SinglyLinkedList
+        words = SinglyLinkedList()
+        words.append('egg')
+        words.append('ham')
+        words.append('bacon')
+        current = words.tail
+        # Simple method to print list output
+        for word in words.iter():
+            print(word)
+        # print List count without needing to iterate over each element in list. taking running time from O(n) to O(1).
+        print("Word count: " + str(words.count))
+        words.delete('ham')
+        print("After deleting word, word count is: " + str(words.count))
+        # Find a word in the list using the iter method. Returns True if found False if missing.
+        if words.search('bacon'):
+            print("found bacon in the list.")
+        print('/--- Singly Linked list end')
 
